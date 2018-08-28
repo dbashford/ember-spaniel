60b4689dcea4c4e039ac02c4d7d31ca0474b654d
asakusuma <asa.kusuma@gmail.com>
2018-08-28 16:12:49 -0700

    Debug prepublish

--- prepublish.js
+++ prepublish.js
@@ -2,4 +2,9 @@
 const rollup = require('ember-rollup/src/prebuild'); 
 const addonPath = __dirname; 
  
-rollup.preBuild(addonPath);
+rollup.preBuild(addonPath).then(() => {
+  setTimeout(() => {
+    console.log('active requests', process._getActiveRequests());
+    console.log('handlers', process._getActiveHandles());
+  }, 1000);
+});
